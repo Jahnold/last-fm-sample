9430ce02177ea483a07d19d3a9ba99959ae879a2
Matt Arnold <matt@boppl.me>
2019-03-24 22:13:48 +0000

    need to inflate the view!

--- feature-search/src/main/java/com/jahnold/lastfmsample/search/view/SearchFragment.kt
+++ feature-search/src/main/java/com/jahnold/lastfmsample/search/view/SearchFragment.kt
@@ -1,7 +1,9 @@
 package com.jahnold.lastfmsample.search.view
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import androidx.lifecycle.ViewModelProvider
@@ -23,6 +25,10 @@ class SearchFragment: BaseFragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(SearchViewModel::class.java)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- feature-search/src/main/java/com/jahnold/lastfmsample/search/viewmodel/SearchViewModel.kt
+++ feature-search/src/main/java/com/jahnold/lastfmsample/search/viewmodel/SearchViewModel.kt
@@ -1,14 +1,20 @@
 package com.jahnold.lastfmsample.search.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.jahnold.lastfmsample.base.navigation.NavigationService
+import com.jahnold.lastfmsample.base.navigation.Navigator
 import com.jahnold.lastfmsample.base.search.SearchService
 import javax.inject.Inject
 
 class SearchViewModel @Inject constructor(
-    private val searchService: SearchService
+    private val searchService: SearchService,
+    private val navigationService: NavigationService
 ): ViewModel() {
 
     fun search(album: String?) {
-        album?.let { searchService.search(it) }
+        album?.let {
+            searchService.search(it)
+            navigationService.sendNavigationEvent(Navigator.Fragments.List)
+        }
     }
 }
